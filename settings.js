db361d08cdfb262518ab7129daa9719876da74d0
icaballero <isael.caballero@exte.com>
2025-05-27 01:49:25 -0600

    feat: ready to test

--- settings.js
+++ settings.js
@@ -5,4 +5,138 @@ const CHAPTERS = [
   { scenes: ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp' ], locked: true, id: 'chapter4', title: 'Capitulo 4' },
 ]
 
-export { CHAPTERS }
+const MERCH = [
+  {
+    name: "Gorra de Altair",
+    price: 250,
+    id: '00001',
+    description: "Para quienes creen que cada estrella guarda una historia.Gorra blanca con estrella bordada al frente y logo Cartas a Altair en la parte trasera.",
+    photos: [
+      "gorra-1.png",
+      "gorra-2.png",
+    ]
+  },
+  {
+    name: "T-Shirt Lazo Eterno",
+    price: 320,
+    description: "Para quienes creen que el lazo entre estrellas es eterno.Camiseta blanca con ilustración trasera, logo al frente, estampado DTF y tela de algodón.",
+    size: ['S', 'M', 'L', 'XL', 'XXL'],
+    id: '00002',
+    photos: [
+      "camisa-1.png",
+      "camisa-2.png",
+    ]
+  },
+  {
+    name: "Totebag Camino Estelar",
+    price: 100,
+    id: '00003',
+    description: "Para quienes siguen su camino hacia nuevas estrellas. Tote bag de color crudo de algodón, resistente, ligera y con estilo para acompañarte en el día a día.",
+    photos: [
+      "totebag-1.png",
+    ]
+  },
+  {
+    name: "Llavero Altair",
+    price: 70,
+    id: '00004',
+    description: "Un toque del universo de Altair para llevar contigo. Llavero morado con logo de Cartas a Altair, hecho de acrílico resistente y aro metálico.",
+    photos: [
+      "llavero-1.png",
+    ]
+  },
+  {
+    name: "Pin Altair",
+    price: 20,
+    id: '00006',
+    description: "Añade un guiño brillante a tu historia. Pin morado de metal esmaltado con el logo de Cartas a Altair. Resistente y lleno de estilo.",
+    photos: [
+      "pin-altair.png",
+    ]
+  },
+  {
+    name: "Pin Carta de Altair",
+    price: 20,
+    id: '00007',
+    description: "Añade un guiño brillante a tu historia. Pin rosado de metal esmaltado con el diseño de la carta de Cartas a Altair. Resistente y lleno de estilo.",
+    photos: [
+      "pincarta-altair.png",
+    ]
+  },
+  {
+    name: "Pin Lazo Eterno",
+    price: 20,
+    id: '00008',
+    description: "Añade un guiño brillante a tu historia.Pin claro de metal esmaltado con la ilustración de Itara y su abuelo.Resistente y lleno de estilo.",
+    photos: [
+      "pinlazo-altair.png",
+    ]
+  },
+  {
+    name: "Funda Estelar",
+    price: 350,
+    description: "Para quienes llevan las estrellas con ellas.Funda transparente con estrellas, diseñada para proteger tu dispositivo con un toque celestial.",
+    id: '00009',
+    photos: [
+      "fundaestelar-altair.png",
+    ],
+    variants: [
+      'iPhone 16 Pro Max',
+      'iPhone 16 Pro',
+      'iPhone 15 Pro Max',
+      'iPhone 15 Pro',
+      'iPhone 14',
+      'iPhone 13',
+      'iPhone 12',
+    ]
+  },
+  {
+    name: "Funda Constelación",
+    price: 350,
+    id: '00010',
+    description: "Para quienes llevan las estrellas con ellas. Funda transparente con estrellas, diseñada para proteger tu dispositivo con un toque celestial.",
+    photos: [
+      "fundaconstelacion-altair.png",
+    ],
+    variants: [
+      'iPhone 16 Pro Max',
+      'iPhone 16 Pro',
+      'iPhone 15 Pro Max',
+      'iPhone 15 Pro',
+      'iPhone 14',
+      'iPhone 13',
+      'iPhone 12',
+    ]
+  },
+  {
+    name: "Stickers de Altair",
+    price: 20,
+    id: '000011',
+    description: "Para los que hacen del arte parte de su día a día. Paquete con 10 stickers ilustrados, perfectos para personalizar tu espacio y objetos favoritos.",
+    photos: [
+      "stickers-altair.png",
+    ]
+  },
+  {
+    name: "Photocards de Altair",
+    price: 20,
+    id: '000110',
+    description: "Lleva contigo un instante del universo de Itara. Paquete con dos photocards de Itara y los personajes principales. Perfectas para coleccionar o regalar.",
+    photos: [
+      "photocard-altair.png",
+    ]
+  },
+  {
+    name: "Cartas a Altair: Novela Completa",
+    price: 400,
+    id: '00000',
+    unlockbook: true,
+    hasGifs: true,
+    description: "Tu viaje empieza aquí. La novela completa te permite sumergirte de principio a fin en la historia de Itara. Ideal para quienes quieren vivir la experiencia completa sin interrupciones.",
+    photos: [
+      "portadanovela-altair.png",
+    ]
+  },
+]
+
+export { CHAPTERS, MERCH }
---
+++ src/pages/Store.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from 'react-router';
+import { MERCH } from '../../settings';
+
+function Store() {
+  /* const chapterOne = CHAPTERS[0];
+ */
+  return (
+    <>
+      <section className="w-full flex flex-col justify-center items-center h-screen min-h-screen bg-cover bg-center bg-no-repeat bg-[url(/assets/mobile/stars_bg.png)] xl:bg-[url(/assets/desktop/stars_bg.png)]">
+        <img className="w-full max-w-[350px] lg:max-w-[550px] xl:max-w-[750px]" src="/assets/store_title.svg" />
+        <div className='p-10 flex gap-2 md:gap-4'>
+          <NavLink className='text-nowrap font-planc px-8 rounded-full border-1 text-[#e67ea7] border-[#e67ea7] hover:text-[black] hover:bg-[#e67ea7] cursor-pointer' to="/">Ir al inicio</NavLink>
+          <button className='text-nowrap font-planc px-8 rounded-full border-1 text-[#e67ea7] border-[#e67ea7] hover:text-[black] hover:bg-[#e67ea7] cursor-pointer'>Ver carrito</button>
+        </div>
+      </section>
+      <div className="relative w-full h-[200px] bg-cover bg-top bg-no-repeat bg-[url(/assets/paper_onli.png)] z-10" />
+      <section className="relative *:w-full min-h-screen bg-[url(/assets/paper.svg)] pb-[150px]">
+        <div className="w-full m-auto max-w-[1000px] grid grid-cols-1 md:grid-cols-3 gap-3">
+          {
+            MERCH.map((product) => {
+              return <div key={ product.id } className="col-span-1 p-2">
+                <div className="w-full overflow-hidden">
+                  <img className="w-full" src={ `/products/${product.photos[0]}` } />
+                </div>
+                <div className="w-full pt-1 flex flex-col items-center">
+                  <span className="font-planc my-1 text-[1.4rem] md:text-[1.6rem] text-center leading-6">
+                    { product.name }
+                  </span>
+                  <span className="font-planc text-[1rem] md:text-[1.2rem] text font-planc-thin my-2">
+                    ${ product.price   } MXN
+                  </span>
+                  <NavLink
+                    className='text-nowrap font-planc px-8 rounded-full border-1 text-[#e67ea7] border-[#e67ea7] hover:text-[white] hover:bg-[#e67ea7] cursor-pointer'
+                    to={ `/store/${product.id}` }>
+                    Comprar
+                  </NavLink>
+                </div>
+              </div>
+            })
+          }
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Store
